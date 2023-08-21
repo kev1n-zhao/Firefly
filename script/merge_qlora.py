c262a1670a548ad27eceaf025b3afc4390d31230
kevin <kevin@kevindeMacBook-Air.local>
2023-08-21 12:44:52 -0700

    u

--- script/merge_qlora.py
+++ script/merge_qlora.py
@@ -33,14 +33,16 @@ def merge_lora_to_base_model():
         max_memory=max_memory,
         torch_dtype=torch.float16,
         trust_remote_code=True,
-        quantization_config=BitsAndBytesConfig(
-            load_in_4bit=True,
-            bnb_4bit_compute_dtype=torch.float16,
-            bnb_4bit_use_double_quant=True,
-            bnb_4bit_quant_type="nf4",
-            llm_int8_threshold=6.0,
-            llm_int8_has_fp16_weight=False,
-        ),
+        load_in_8bit=False
+
+        # quantization_config=BitsAndBytesConfig(
+        #     load_in_4bit=True,
+        #     bnb_4bit_compute_dtype=torch.float16,
+        #     bnb_4bit_use_double_quant=True,
+        #     bnb_4bit_quant_type="nf4",
+        #     llm_int8_threshold=6.0,
+        #     llm_int8_has_fp16_weight=False,
+        # ),
     )
     model = PeftModel.from_pretrained(model, adapter_name_or_path)
     model = model.merge_and_unload()
